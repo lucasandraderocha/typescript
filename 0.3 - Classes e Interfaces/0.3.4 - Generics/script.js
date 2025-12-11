bb0ee0eb88d87751884c21e72618156216bd70d8
lucasandraderocha <lucasandraderocha@proton.me>
2025-12-11 18:00:36 -0300

    estudos: Estudando um pouco sobre Functions no Typescript e realizando algumas ativadades para fixar o conteúdo

--- 0.3 - Classes e Interfaces/0.3.4 - Generics/script.js
+++ 0.3 - Classes e Interfaces/0.3.4 - Generics/script.js
@@ -1,36 +1,62 @@
-//  function sum<G>(numA: G): G {
-//   return numA
-//  }
-//  const summed = sum<boolean>(true)
-//  console.log(summed)
-//  const numeros = [1,2,3,4,5,6,7]
-//  const frutas = ["Maçã","Pêra","Melância","Abacaxi","Abacate","Uva","Banana"]
-//  const firstFive = <G>(lista: G[]) => {
-//   return lista.slice(0, 5);
-//  }
-//  console.log(firstFive(numeros))
-//  console.log(firstFive(frutas))
-const p = document.createElement("p");
-const button = document.createElement("button");
-p.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi temporibus similique harum voluptate, nulla impedit adipisci quisquam eos commodi inventore asperiores laboriosam, corrupti, nemo eaque quas nobis hic doloribus debitis!
-    Dicta nemo fugiat, ipsum nulla harum doloremque ullam, aliquid earum, corrupti accusamus atque. Nobis rerum blanditiis asperiores porro. Exercitationem eum quidem provident itaque est libero non aspernatur possimus vel quia.
-    Animi et corporis excepturi sed qui pariatur inventore quod mollitia hic enim ipsa sequi dolorem, doloribus, perferendis ducimus praesentium optio! Quos tenetur corporis est voluptatibus tempora, quia qui laudantium repellendus.
-    Qui assumenda, velit molestias ad labore nihil, amet eum quibusdam excepturi officiis, magni sequi deleniti quaerat corrupti repellat perspiciatis harum. Eveniet harum aperiam aliquid veritatis a cumque rem, non quas.
-    Voluptate perspiciatis officia laborum repellat. Corrupti accusantium voluptate culpa mollitia. Libero amet ipsam sit deserunt similique quaerat eos? Illo itaque expedita quo totam alias consequuntur nihil, debitis quae ex distinctio.`;
-button.innerText = "Click me";
-const extractText = (el) => {
-    return { text: el.textContent, el };
+// const logGeneric = <T>(arg: T): T => {
+//   return arg
+// }
+// console.log(logGeneric<string[]>(["Maçã", "Pêra", "Melância"]))
+// console.log(logGeneric<number[]>([9,9,8]))
+// console.log(logGeneric<object>({list: "list"}))
+// const numeros = [1,2,3,4,5,6,7,8]
+// const frutas = ["fruta a", "fruta b", "fruta c ", "frutas d", "frutas e", "frutas f"]
+// console.log({
+//   numeros, frutas
+// })
+// const firstFive = <T>(list: T[]): T[] => {
+//   return list.slice(0,4)
+// }
+// console.log(firstFive<number>(numeros))
+// console.log(firstFive<string>(frutas))
+// const typeOf = () => {
+//   let types = {}
+//   const linkType = <T>(data: T): object => ({
+//     data,
+//     type: typeof data
+//   })
+//   return {
+//     get types() {return types},
+//     type: <T>(data: T): object => linkType(data),
+//   }
+// }
+// const x = typeOf()
+// console.log(x.type("baralho"))
+// console.log(x.type(2))
+// const element = document.createElement("div")
+// element.classList.add("container")
+// element.innerHTML = "Hello, world."
+// const extractTextElement = <T extends HTMLElement>(el: T) => {
+//   return el
+// }
+// function _<T extends Element>(selector: string): T | null {
+//   return document.querySelector(selector)
+// }
+// const div = _<HTMLDivElement>(".container")?.dataset
+// console.dir(div)
+const create = (argv) => {
+    return document.createElement(argv);
 };
-console.log(extractText(p));
-console.log(extractText(button));
+const div = create("div");
 const getData = async (url) => {
     const res = await fetch(url);
     return await res.json();
 };
-const getProduct = async () => {
-    const notebook = await getData("https://api.origamid.dev/json/notebook.json");
-    console.log({ nome: notebook.nome, preco: notebook.preco });
-};
-getProduct();
+const notebook = await getData("https://api.origamid.dev/json/notebook.json");
+if (div) {
+    div.innerHTML = String(new Date().toLocaleDateString("pt-BR", {
+        weekday: "short",
+        year: "numeric",
+        month: "numeric",
+        day: "numeric"
+    }));
+    div.innerHTML += notebook.nome;
+    document.body.appendChild(div);
+}
 export {};
 //# sourceMappingURL=script.js.map
--- 0.3 - Classes e Interfaces/0.3.5 - Functions/script.ts
+++ 0.3 - Classes e Interfaces/0.3.5 - Functions/script.ts
@@ -1,56 +1,19 @@
-//  function sum<G>(numA: G): G {
-//   return numA
-//  }
+// Function Overloads 
+function unityConverter(num: number): string;
+function unityConverter(num: number, unity: "mm"| "cm"): number;
+function unityConverter(num: number, unity?: "mm" | "cm"): number | string {
+  if(typeof unity === "undefined") {
 
-//  const summed = sum<boolean>(true)
+    return `Resultado: ${num}mm`
+  }
 
-//  console.log(summed)
+  const unities = {
+    mm: {result: num * 10, unity: "mm"},
+    cm: {result: num / 10, unity: "cm"},
+  }
 
-
-//  const numeros = [1,2,3,4,5,6,7]
-//  const frutas = ["Maçã","Pêra","Melância","Abacaxi","Abacate","Uva","Banana"]
-
-//  const firstFive = <G>(lista: G[]) => {
-//   return lista.slice(0, 5);
-//  }
-
-
-//  console.log(firstFive(numeros))
-//  console.log(firstFive(frutas))
-
- const p = document.createElement("p")
- const button = document.createElement("button")
-
- p.textContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Excepturi temporibus similique harum voluptate, nulla impedit adipisci quisquam eos commodi inventore asperiores laboriosam, corrupti, nemo eaque quas nobis hic doloribus debitis!
-    Dicta nemo fugiat, ipsum nulla harum doloremque ullam, aliquid earum, corrupti accusamus atque. Nobis rerum blanditiis asperiores porro. Exercitationem eum quidem provident itaque est libero non aspernatur possimus vel quia.
-    Animi et corporis excepturi sed qui pariatur inventore quod mollitia hic enim ipsa sequi dolorem, doloribus, perferendis ducimus praesentium optio! Quos tenetur corporis est voluptatibus tempora, quia qui laudantium repellendus.
-    Qui assumenda, velit molestias ad labore nihil, amet eum quibusdam excepturi officiis, magni sequi deleniti quaerat corrupti repellat perspiciatis harum. Eveniet harum aperiam aliquid veritatis a cumque rem, non quas.
-    Voluptate perspiciatis officia laborum repellat. Corrupti accusantium voluptate culpa mollitia. Libero amet ipsam sit deserunt similique quaerat eos? Illo itaque expedita quo totam alias consequuntur nihil, debitis quae ex distinctio.`
-
-
-button.innerText = "Click me"
-    
-const extractText = <T extends HTMLElement> (el: T) => {
-      return {text: el.textContent,el}
+  return unities[unity].result
 }
 
-console.log(extractText(p))
-console.log(extractText(button))
-
-const getData = async <T>(url: string): Promise<T> => {
-  const res = await fetch(url)
-  return await res.json()
-}
-
-interface Notebook {
-  nome: string;
-  preco: number;
-}
-
-const getProduct = async () => {
-  const notebook = await getData<Notebook>("https://api.origamid.dev/json/notebook.json")
-  console.log({nome: notebook.nome, preco: notebook.preco})
-}
-
-
-getProduct()
+console.log(unityConverter(9, "cm"))
+console.log(unityConverter(9))
